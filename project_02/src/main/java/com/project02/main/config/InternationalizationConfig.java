893e845266c44cdb5b955d8ae5b77ff6269cb42e
Richard Danis <richard0135@gmail.com>
2018-05-03 18:27:22 +0200

    eddig_csinalom_a_dolgomat

--- project_02/src/main/java/com/project02/main/config/InternationalizationConfig.java
+++ project_02/src/main/java/com/project02/main/config/InternationalizationConfig.java
@@ -1,21 +1,21 @@
 package com.project02.main.config;
 
+import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class AppConfig extends WebMvcConfigurerAdapter {
+public class InternationalizationConfig {
 
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-		// sessionLocaleResolver.setDefaultLocale(Locale.US);
+		//sessionLocaleResolver.setDefaultLocale(Locale.US);
 		return sessionLocaleResolver;
 	}
 
@@ -26,7 +26,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		return lci;
 	}
 
-	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
