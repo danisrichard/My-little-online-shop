0c8147632881c88ba688c33c021929c4600f4ab2
Richard Danis <richard0135@gmail.com>
2018-05-15 11:33:33 +0200

    pagination html

--- project_02/src/main/java/com/project02/main/controller/IndexPageController.java
+++ project_02/src/main/java/com/project02/main/controller/IndexPageController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
@@ -22,19 +22,17 @@ public class IndexPageController {
 	private ProductService productService;
 
 	@GetMapping("/home")
-	public ModelAndView loadIndexPage(@RequestParam("page") Optional<Integer> pageNumb) {
+	public String loadIndexPage(@RequestParam("page") Optional<Integer> pageNumb,Model model) {
 
 		int ePage = (pageNumb.orElse(0) < 1) ? INITIAL_PAGE : pageNumb.get()-1;
 
-		Page<Product> products = productService.findAllProductPage(new PageRequest(ePage, 5));
+		Page<Product> products = productService.findAllProductPage(PageRequest.of(ePage, 5));
 		Pager pager = new Pager(products);
 
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("products", products);
-		modelAndView.addObject("pager", pager);
-		modelAndView.setViewName("/index");
+		model.addAttribute("products",products);
+		model.addAttribute("pager",pager);
 
-		return modelAndView;
+		return "/index";
 	}
 	
 }
